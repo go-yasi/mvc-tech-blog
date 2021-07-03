1ea977e70f2fc12dc3e793c4cc1fd92e30120b29
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-02 21:28:06 -0700

    Required userRoutes in api/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const commentRoutes = require('./commentRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/comment', commentRoutes);
+router.use('/user', userRoutes);
 
 module.exports = router;

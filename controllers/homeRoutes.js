0300579f48c90e0d0a3e6281e51cf0ad25dc124d
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-02 17:24:15 -0700

    Created a post route on postRoutes to get a blog post by id. Created post.handlebars page to display individual post details. Temporarily removed CSS from main

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,21 +1,14 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models')
 
-// router.get('/', async (req,res) => {
-//     try {
-//         res.render('homepage');
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
-
+// GET data for all posts to generate on home page
 router.get('/', async (req, res) => {
     try {
       const postData = await Post.findAll();
   
       const blog_posts = postData.map((info) => info.get({ plain: true }));
   
-      res.render('homepage', {
+      res.render('home', {
         blog_posts
       });
     } catch (err) {
--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -1,21 +1,27 @@
-// const router = require('express').Router();
-// const { Post, User, Comment } = require.apply('../models');
-// // const withAuth =  require('.//utils/auth;');
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+// const withAuth =  require('.//utils/auth;');
 
-// // GET blog posts
-// router.get('/', async (req, res) => {
-//     try {
-//         const postData = await Post.findAll({
-//             include: [{ model: User }],
-//         });
-//         const blog_posts = postData.map((post) => 
-//         post.get({plain: true}));
-//         res.render('homepage', {
-//             blog_posts
-//         });
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+// GET blog post by id
+router.get('/:id', 
+// withAuth, 
+async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [{ model: User}, {model: Comment}]
+        });
+        if (!postData) {
+            res.status(404).json({ message: "This post does not exist"});
+            return;
+        }
+        const post = postData.get({plain: true});
+        res.render('post', {post
+            // , logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
-// module.exports = router;
+
+module.exports = router;

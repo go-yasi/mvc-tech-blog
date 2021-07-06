8f42ab36dd63e7c36e9ed18b47165b9132e77696
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-05 22:07:48 -0700

    Created edit-post.js to update blog posts

---
+++ public/js/edit-post.js
@@ -0,0 +1,23 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const title = document.querySelector('#edit-post-title').value.trim();
+    const content = document.querySelector('#edit-post-content').value.trim();
+    // need to get time of submit for 'updated at'
+
+    const response = await fetch('/api/post', {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            content
+        })
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashbaord');
+    } else {
+        alert('Update failed. Please try again.');
+    }
+};
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);

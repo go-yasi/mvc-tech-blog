23a638b3cfe9f5d7d0a5523a756b916907d4a5d4
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-05 18:15:04 -0700

    Added dashRoutes.js to create routes for dashboard and connected in api/index.js

--- controllers/api/dashRoutes.js
+++ controllers/api/dashRoutes.js
@@ -1,3 +1,9 @@
 ////////////////////////////
 //  ROUTES for /api/dash  //
 ////////////////////////////
+
+const router = require('express').Router();
+const { User, Post, Comment } = require('../../models');
+
+
+module.exports = router;

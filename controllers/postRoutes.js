2f666b4dbef3b5cb6970ad52e00a4076a127ce37
Yasi Moshtael <yasi.moshtael@gmail.com>
2021-07-07 16:14:45 -0700

    Added a new post route to postRoutes to create new post. Not working - new post isn't being added to DB

--- controllers/postRoutes.js
+++ controllers/postRoutes.js
@@ -26,4 +26,20 @@ async (req, res) => {
     }
 });
 
+// Create new post
+router.post('/', 
+// withAuth, 
+async (req, res) => {
+    try {
+        const postData = await Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            user_id: req.session.user_id
+        });
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
